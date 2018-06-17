4e42c36d18215734a1e9dbae3fe7cc344e58bd88
aakash1147 <aakash1147@live.com>
2018-06-17 18:18:53 +0530

    user models

---
+++ backend/models/users.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+
+var UserSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email Id is required"],
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: `{value} is not a valid email`,
+        }
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    }
+});
+
+var User = mongoose.model('user', UserSchema);
+module.exports = User;
--- server.js
+++ server.js
@@ -1,11 +1,31 @@
+const _ = require('lodash');
 const express = require('express');
+const bodyParse = require('body-parser');
 const mongoose = require('./backend/db/dbConnection');
 
-var URL = require('url');
+
+
+var User = require('./backend/models/users');
+
+
 
 var app = express();
-
 
 app.listen(8000, ()=> {
     console.log("Server is running on port 8000")
 });
+
+
+app.use(bodyParse.json());
+
+app.post('/create/user', (req, res) => {
+    var reqBody = _.pick(req.body, ['email', 'password']);
+    console.log(reqBody);
+
+    var user = new User(reqBody);
+
+    user.save().then((err, user) => {
+        if(err) res.status(400).send(err);
+        else res.status(200).send(user);
+    })
+})
