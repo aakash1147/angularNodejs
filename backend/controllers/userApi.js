348e2fcd3e939a6574128fa8ebbb240dc3a91fe8
aakash1147 <aakash1147@live.com>
2018-06-22 23:30:49 +0530

    returin err in create user done

--- backend/controllers/userApi.js
+++ backend/controllers/userApi.js
@@ -3,15 +3,15 @@ const _ = require('lodash');
 var userModel = require('../models/userModel');
 
 var createUser = function(req, res) {
-    console.log(req.body);
     var body = _.pick(req.body, ['email', 'password']);
     
     var user = new userModel(body);
 
     user.save().then((err, user) => {
-        if(err) res.status(400).send(err);
-        else res.status(200).send(user);
-    });
+        res.status(200).send(user);
+    }).catch((err) => {
+        res.status(400).send(err);
+    }) ;
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -13,16 +13,3 @@ app.listen(8000, ()=> {
 });
 
 
-
-
-// app.post('/create/user', (req, res) => {
-//     var reqBody = _.pick(req.body, ['email', 'password']);
-//     console.log(reqBody);
-
-//     var user = new User(reqBody);
-
-//     user.save().then((err, user) => {
-//         if(err) res.status(400).send(err);
-//         else res.status(200).send(user);
-//     })
-// })
