fa728b41e01b626cfc8c2fe379568e5c23753cf1
aakash1147 <aakash1147@live.com>
2018-06-23 00:51:41 +0530

    encypted password is save one the user is creted

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -35,23 +35,45 @@ var UserSchema = mongoose.Schema({
 
 
 UserSchema.methods.toJSON = function () {
+    // for returning limited date to the API
     var user = this;
     var userObject = user.toObject();
    return _.pick(userObject, ['_id', 'email']);
 };
   
 UserSchema.methods.generateAuthToken = function () {
+    // for generating auth token
     var user = this;
-    var access = 'auth';
-    
+    var access = 'auth';    
     var token = jwt.sign({_id: user._id.toHexString(), access}, 'akash1147').toString();
-
     user.tokens.push({access, token});
-
     return user.save().then(() => {
         return token;
     });
+};
+
+UserSchema.methods.findByCrediential = function (email, password) {
+    // to find the user by crediential email id
+    
+
 }
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+    if(user.isModified('password')) {
+        // to check if password is modified than modified it encrypted password
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
+
+
 
 
 var UserModel = mongoose.model('user', UserSchema);
